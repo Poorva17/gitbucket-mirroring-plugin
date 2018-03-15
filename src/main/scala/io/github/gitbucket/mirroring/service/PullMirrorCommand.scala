5722d1b4b9741de478e055758fd435214eea74ba
Poorva Gokhale <poorvasgokhale@gmail.com>
2018-03-15 16:49:50 +0530

    fix pullcommand

--- src/main/scala/io/github/gitbucket/mirroring/service/PullMirrorCommand.scala
+++ src/main/scala/io/github/gitbucket/mirroring/service/PullMirrorCommand.scala
@@ -20,31 +20,17 @@ class PullMirrorCommand(repo: Repository) extends TransportCommand[PullMirrorCom
   override def call(): Unit = {
 
     val mirrorRefSpec = new RefSpec("+refs/*:refs/*")
-    println("*" * 50 + repo)
-    println("*" * 50 + remote)
     val git = new Git(repo)
 
     // The mirror ref spec is not enough to propagate local deleted references. Therefore we use the remote ls
     // command to find all the deleted ref specs and add them explicitly.
 
-    val lsRemoteCommand = git.lsRemote()
-      .setRemote(remote)
-      .setHeads(true)
-      .setTags(true)
-
-    configure(lsRemoteCommand)
-
-    val deletedRefSpecs = lsRemoteCommand.call()
-      .asScala
-      .withFilter { ref => repo.findRef(ref.getName) == null }
-      .map { ref => new RefSpec(s":${ref.getName}") }
-
     val pullCommand = git.fetch()
       .setRemote(remote)
-      .setRefSpecs(((mirrorRefSpec :: Nil) ++ deletedRefSpecs).asJava)
+      .setRefSpecs(Seq(mirrorRefSpec).asJava)
 
-    println("aaaa")
     configure(pullCommand)
+    pullCommand.call()
   }
 
 }
